40b4f446c3ea01c51b26c0cde2ecd4c1dd5cf6f9
VWGSROW <roland.wagner@volkswagen-groupservices.com>
2025-06-24 17:52:39 +0200

    Update Filegenerator.ps1

--- Filegenerator.ps1
+++ Filegenerator.ps1
@@ -1,29 +1,30 @@
 Measure-Command{
-$bigFileName = "plc_log.txt"
-$plcNames = 'PLC_A','PLC_B','PLC_C','PLC_D'
+$bigFileName = (Get-Location).Path +"\plc_log.txt"
+$plcNames = @('PLC_A','PLC_B','PLC_C','PLC_D')
 $errorTypes = @(
     'Sandextrator overload',
     'Conveyor misalignment',
     'Valve stuck',
     'Temperature warning'
 )
-$statusCodes = 'OK','WARN','ERR'
- 
-$logLines = @()
- 
+$statusCodes = @('OK','WARN','ERR')
+$list = [System.Collections.Generic.List[string]]::new(50000)
+$randomObject = New-Object -TypeName System.Random
+$date = Get-Date
+
 for ($i=0; $i -lt 50000; $i++) {
-    $timestamp = (Get-Date).AddSeconds(-$i).ToString("yyyy-MM-dd HH:mm:ss")
-    $plc = $plcNames | Get-Random
-    $operator = Get-Random -Minimum 101 -Maximum 121
-    $batch = Get-Random -Minimum 1000 -Maximum 1101
-    $status = $statusCodes | Get-Random
-    $machineTemp = [math]::Round((Get-Random -Minimum 60 -Maximum 110) + (Get-Random),2)
-    $load = Get-Random -Minimum 0 -Maximum 101
- 
-    if ((Get-Random -Minimum 1 -Maximum 8) -eq 4) {
-        $errorType = $errorTypes | Get-Random
+    $timestamp = "{0:yyyy-MM-dd HH:mm:ss}" -f $date.AddSeconds(-$i)
+    $plc = $plcNames[$randomObject.Next(0,3)]
+    $operator = $randomObject.Next(101,121)
+    $batch = $randomObject.Next(1000,1101)
+    $status = $statusCodes[$randomObject.next(0,2)]  
+    $machineTemp = [math]::Round(($randomObject.next(60,110)) + ($randomObject.next()),2) 
+    $load = $randomObject.Next(0,101)
+
+    if ($randomObject.next(1,8) -eq 4) {
+        $errorType = $errorTypes[$randomObject.next(0,3)] 
         if ($errorType -eq 'Sandextrator overload') {
-            $value = (Get-Random -Minimum 1 -Maximum 11)
+            $value = $randomObject.next(1,11)
             $msg = "ERROR; $timestamp; $plc; $errorType; $value; $status; $operator; $batch; $machineTemp; $load"
         } else {
             $msg = "ERROR; $timestamp; $plc; $errorType; ; $status; $operator; $batch; $machineTemp; $load"
@@ -31,10 +32,17 @@ for ($i=0; $i -lt 50000; $i++) {
     } else {
         $msg = "INFO; $timestamp; $plc; System running normally; ; $status; $operator; $batch; $machineTemp; $load"
     }
- 
-    $logLines += $msg
+
+    $list.Add($msg)
 }
- 
-Set-Content -Path $bigFileName -Value $logLines
+
+$stream = New-Object IO.StreamWriter $bigFileName, $true
+
+foreach ($item in $list) {
+    $stream.WriteLine($item)
+}
+
+$stream.Close()
+
 Write-Output "PLC log file generated."
 }
